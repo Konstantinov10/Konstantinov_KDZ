37cc2eb62b0aabc0e18a24b16a50c4b8fdd3b176
Иван Константинов <ikonst98@hotmail.com>
2017-04-20 19:34:22 +0300

    Редактирование.

--- Wpf_Konst_Transf/StartManagerWindow.xaml.cs
+++ Wpf_Konst_Transf/StartManagerWindow.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Wpf_Konst_Transf
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для StartManagerWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class StartManagerWindow : Window
     {
-        public MainWindow()
+        public StartManagerWindow()
         {
             InitializeComponent();
-        }
-
-        private void StartPlBut_Click(object sender, RoutedEventArgs e)
-        {
-            var window = new StartPlayerWind();
-            window.ShowDialog();
-           
         }
     }
 }
---
+++ Wpf_Konst_Transf/StartPlayerWind.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wpf_Konst_Transf
+{
+    /// <summary>
+    /// Логика взаимодействия для StartPlayerWind.xaml
+    /// </summary>
+    public partial class StartPlayerWind : Window
+    {
+        public StartPlayerWind()
+        {
+            InitializeComponent();
+            textBoxPLog.GotFocus += TextBoxPLog_GotFocus;
+            textBoxPLog.LostFocus += TextBoxPLog_LostFocus;
+            textBoxPpassw.GotFocus += TextBoxPpassw_GotFocus;
+            textBoxPpassw.LostFocus+=TextBoxPpassw_LostFocus;
+        }
+        bool _loginEntered = false;
+        bool _loginEntered2 = false;
+        private void TextBoxPLog_GotFocus(object sender, EventArgs e)
+        {
+            if (!_loginEntered)
+            {
+                textBoxPLog.Text = "";
+                textBoxPLog.Foreground = new SolidColorBrush(Colors.Black);
+            }
+        }
+        private void TextBoxPLog_LostFocus(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBoxPLog.Text))
+                _loginEntered = true;
+            else
+            {
+                textBoxPLog.Text = "Login";
+                _loginEntered = false;
+                textBoxPLog.Foreground = new SolidColorBrush(Colors.Gray);
+            }
+        }
+
+        private void textBoxPLog_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBoxPpassw_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (!_loginEntered2)
+            {
+                textBoxPpassw.Text = "";
+                textBoxPpassw.Foreground = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        private void TextBoxPpassw_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBoxPpassw.Text))
+                _loginEntered2 = true;
+            else
+            {
+                textBoxPpassw.Text = "Password";
+                _loginEntered2 = false;
+                textBoxPLog.Foreground = new SolidColorBrush(Colors.Gray);
+            }
+        }
+
+        private void textBoxPpassw_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+    } 
+}
